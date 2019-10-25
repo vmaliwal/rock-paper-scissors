88bb3c8dc7af26aea3fc941b8b7f0555c7ac2c6f
Varun Maliwal <dev@varun.io>
2019-10-25 09:55:26 +0530

    Adding basic cli for invoking the game through bash

--- cli.js
+++ cli.js
@@ -1,17 +1,86 @@
+const {playGame, pickRandomElement, elements} = require('./index.js')
+
 const readline = require('readline')
 
 const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout,
-    prompt: 'VARUN> '
+    output: process.stdout
 })
 
-rl.prompt()
+const startQuestionText = () => {
+    const text = `
+    Let's play Rock, Paper & Scissors --
+    
+    Choose from available options:
+    
+    1. Player 1 vs Computer (Player 2)
+    2. Computer (Player 1) vs Computer (Player 2)
+    
+    `
+    return text
+}
 
-rl.question(`Choose from available option:
-1. Player vs Computer
-2. Computer vs Computer
+const playWithComputerText = (el = elements) => {
 
-`, (answer) => {
-    console.log(`You chose ${answer}`)
-})
+    const arr = el.map((e, i) => {
+        return ` ${i + 1}. ${e.toUpperCase()}`
+    })
+
+    const text = `
+    
+    Player 1, choose from available options :
+    
+    ${arr.join(",")}
+    
+    `
+    return text
+}
+
+const questionFn = (question) => {
+    return new Promise( (res) => {
+        rl.question(question, answer => {
+            res(answer)
+        })
+    })
+}
+
+const playerOneChoice = async () => {
+    let playerChoice = await questionFn(playWithComputerText())
+    playerChoice = Number(playerChoice)
+
+    return playerChoice
+}
+
+(async function main() {
+    let answer = await questionFn(startQuestionText())
+    answer = Number(answer)
+
+    switch(answer) {
+        case 1: {
+            console.log(`Thank you for choosing option ${answer}. Player 1 please commence`) 
+
+            let choice = -1
+
+            while (choice < 0 || choice > elements.length) {
+                                
+                choice = await playerOneChoice()
+
+
+                if (choice < 0 || choice > elements.length)
+                    console.log("Please choose a valid option")
+
+            }
+
+            console.log(`Thank you for choosing ${choice}. The winner of your game is ${playGame(choice, pickRandomElement())}`)             
+            break
+        }
+        case 2: {
+            console.log(`Thank you for choosing ${answer}. The winner of your game is ${playGame(pickRandomElement(), pickRandomElement())}`)            
+            break
+        }
+        default: {
+            main()
+            break
+        }
+    }
+})()
--- index.js
+++ index.js
@@ -1,47 +1,48 @@
-(() => {
 
-    const gameChoice = ['rock', 'paper', 'scissors']
+/**
+ * Returns array of game elements to play
+ */
+const gameElements = () => ['rock', 'paper', 'scissors']
+/**
+ * 
+ * @param {Array} choice - array of choices available to select  
+ */
+const pickRandom = (choice = gameElements()) => {
+    return Math.floor(Math.random() * choice.length)
+}
 
+/**
+ * 
+ * @param {int} fpChoice 
+ * @param {int} spChoice 
+ */
+const results = (fpChoice, spChoice) => {
     /**
+     * Matrix denotes result set
+     * where x axis is first player
+     * y axis is second player
      * 
-     * @param {Array} choice - array of choices available to select  
+     *   r  p  s
+     * r t p1 p2
+     * p p2 t p1
+     * s p1 p2 t
      */
-    const pickRandom = (choice = gameChoice) => {
-        return Math.floor(Math.random() * choice.length)
+    const resultsArr  = [
+        ['t','p1','p2'],
+        ['p2','t','p1'],
+        ['p1','p2','t']
+    ]
+
+    const resultsObj = {
+        't': 'TIE',
+        'p1': 'Player 1',
+        'p2': 'Player 2'
     }
 
-    /**
-     * 
-     * @param {int} fpChoice 
-     * @param {int} spChoice 
-     */
-    const results = (fpChoice, spChoice) => {
-        /**
-         * Matrix denotes result set
-         * where x axis is first player
-         * y axis is second player
-         * 
-         *   r  p  s
-         * r t p1 p2
-         * p p2 t p1
-         * s p1 p2 t
-         */
-        const resultsArr  = [
-            ['t','p1','p2'],
-            ['p2','t','p1'],
-            ['p1','p2','t']
-        ]
+    const mapResults = resultsArr[fpChoice][spChoice]
 
-        const resultsObj = {
-            't': 'TIE',
-            'p1': 'Player 1 wins',
-            'p2': 'Player 2 wins'
-        }
+    return resultsObj[mapResults]
+}
 
-        const mapResults = resultsArr[fpChoice][spChoice]
-
-        return resultsObj[mapResults]
-    }
-})()
-
+module.exports =  { playGame: results, pickRandomElement: pickRandom, elements: gameElements() }
 
