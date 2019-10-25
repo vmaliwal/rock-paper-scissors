9ad9b87942b32e6ef4c809b88b5bf1f9a5563896
Varun Maliwal <dev@varun.io>
2019-10-25 14:46:58 +0530

    Added README file

---
+++ README.md
@@ -0,0 +1,22 @@
+# Rock Paper Scissors
+
+A small Node.js app to play [Rock Paper Scissors](https://en.wikipedia.org/wiki/Rock%E2%80%93paper%E2%80%93scissors) game in terminal 
+
+## Requirements
+- [NodeJS](https://nodejs.org/en/)
+- [NPM](https://www.npmjs.com/)
+
+## Installation
+```
+$ npm install
+```
+
+## To play
+```
+$ npm start
+```
+
+## Tests
+```
+$ npm test
+```
--- test/test.js
+++ test/test.js
@@ -7,26 +7,26 @@ describe('The elements array should:', () => {
     it('be non-empty', () => {
         expect(elements.length).to.greaterThan(0)
     })
-
+    
     it('contain "rock" element in the array', () => {
         expect(elements).contain('rock')
     })
-
+    
     it('contain "paper" element in the array', () => {
         expect(elements).contain('paper')        
     })
-
+    
     it('contain "scissors" element in the array', () => {
         expect(elements).contain('scissors')
     })
-
+    
 })
 
 describe('The random method should:', () => {
     it('be less than elements.length', () => {
         expect(pickRandomElement()).to.lessThan(elements.length)
     })
-
+    
     it('be greater than 0', () => {
         expect(pickRandomElement() + 1).to.greaterThan(0)
     })
@@ -36,32 +36,32 @@ describe('The playGame method should:', () => {
     it('return undefined if parameters are not provided', () => {
         expect(playGame()).to.equal(undefined)
     })
-
+    
     it('return TIE if both player chooses same elements', () => {
         expect(playGame(0,0)).to.equal('TIE')
     })
-
+    
     it('return Player 1 if Player 1 chooses ROCK and Player 2 chooses SCISSORS', () => {
         expect(playGame(0,2)).to.equal('Player 1 WINS')
     })
-
+    
     it('return Player 2 if Player 1 chooses PAPER and Player 2 chooses SCISSORS', () => {
         expect(playGame(1,2)).to.equal('Player 2 WINS')
     })
-
+    
 })
 
 describe('The isNumber method should:', () => {
     it('be true when checked for 2', () => {
         expect(isNumber(2)).to.be.true
     })
-
+    
     it('be false when checked for undefined', () => {
         expect(isNumber()).to.be.false
     })
-
+    
     it('be false when checked for string "2"', () => {
         expect(isNumber("2")).to.be.false
     })
-
+    
 })
